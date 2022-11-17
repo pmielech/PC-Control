40ddd7ebcfb467eefe829cc90b61d99c0e1dbec1
Patryk Mielech <patryk.mielech@protonmail.com>
2022-11-17 20:07:12 +0100

    Rebuilded project
    * created main while
    * added command dict
    * created help and go to command

--- pc-control.py
+++ pc-control.py
@@ -1,17 +1,110 @@
+import sys
+
 from google.cloud import speech
 import speech_recognition as rec
+import os
 
+global comm
+comm = -1
+global recorded
+global audio
+global response
+run = 0
+state_machine = 0
 
-client = speech.SpeechClient.from_service_account_json('pc-ctrl-key.json')
-user_input = rec.Recognizer()
-
-with rec.Microphone() as source:
-    print("sey")
-    recorded = user_input.listen(source)
-recorded_wav = rec.AudioData.get_wav_data(recorded)
-audio = speech.RecognitionAudio(content=recorded_wav)
-
+curr_path = os.getcwd()
+commands_dict = {"exit": 0, "help": 1, "go to": 2, "make folder": 3, "current path": 4}
 config = speech.RecognitionConfig(sample_rate_hertz=44100, enable_automatic_punctuation=True, language_code='en-US')
+try:
+    client = speech.SpeechClient.from_service_account_json('pc-ctrl-key.json')
+    run = 1
+except FileNotFoundError:
+    print("~Failed to read google account json~")
+    sys.exit()
 
-response = client.recognize(config=config, audio=audio)
-print(response)
+
+def record_data():
+    global recorded
+    with rec.Microphone() as source:
+        print("Listening...\n", end="")
+        recorded = rec.Recognizer().listen(source)
+    convert_to_wav()
+
+
+def convert_to_wav():
+    global audio
+    recorded_wav = rec.AudioData.get_wav_data(recorded)
+    audio = speech.RecognitionAudio(content=recorded_wav)
+
+
+def get_response():
+    global response
+    try:
+        received_response = client.recognize(config=config, audio=audio)
+    except:
+        print("Failed get the response")
+        sys.exit()
+    try:
+        response = ("{}".format(received_response.results[0].alternatives[0].transcript)).lower().rstrip("!.?")
+    except IndexError:
+        print("Empty message")
+        return
+    print(f'\033[92m' + response + '\033[0m')
+
+
+def recognize_comm():
+    global response
+    global comm
+    if response in commands_dict.keys():
+        comm = commands_dict.get(response)
+    else:
+        print("Sorry, can't do that")
+        comm = -1
+        return
+
+
+def go_to_comm():
+    global curr_path
+    global response
+    global comm
+    print(f'\033[93m' + "~GO TO MODE~" + '\033[0m')
+    print("Current path: {}".format(curr_path))
+    record_data()
+    get_response()
+    try:
+        if os.path.exists(os.path.join(curr_path, response)):
+            curr_path = os.path.join(curr_path, response)
+            print("Going to {}".format(curr_path))
+            os.chdir(curr_path)
+        else:
+            print("Sorry directory doesn't exist")
+    except Exception as e:
+        print(e)
+
+
+def print_help():
+    print(f'\033[93m' + "~HELP~"'\033[0m')
+    help_string = "Available commands: "
+    for command in commands_dict:
+        help_string += command + ', '
+    print(f'\033[93m' + help_string.rstrip(", ") + '\033[0m')
+
+
+def execute_comm():
+    global comm
+    if comm == 0:
+        print("Exiting...")
+        sys.exit()
+    elif comm == 1:
+        print_help()
+        return
+    elif comm == 2:
+        go_to_comm()
+        return
+
+
+while run == 1:
+    record_data()
+    get_response()
+    recognize_comm()
+    execute_comm()
